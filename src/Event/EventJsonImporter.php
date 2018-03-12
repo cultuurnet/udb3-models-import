103024a4f834299c3b8a3a31aaf104764cd2cd7b
Bert Ramakers <bertramakers@me.com>
2018-03-12 17:18:58 +0100

    III-2522: Simplify EventJsonImporter by moving validation/pre-processing to other classes

--- src/Event/EventJsonImporter.php
+++ src/Event/EventJsonImporter.php
@@ -5,7 +5,6 @@ namespace CultuurNet\UDB3\Model\Import\Event;
 use Broadway\CommandHandling\CommandBusInterface;
 use CultuurNet\UDB3\Address\Address;
 use CultuurNet\UDB3\Calendar;
-use CultuurNet\UDB3\Category;
 use CultuurNet\UDB3\Event\Commands\CreateEvent;
 use CultuurNet\UDB3\Event\Commands\Moderation\Publish;
 use CultuurNet\UDB3\Event\Commands\UpdateCalendar;
@@ -21,30 +20,21 @@ use CultuurNet\UDB3\Location\Location;
 use CultuurNet\UDB3\Location\LocationId;
 use CultuurNet\UDB3\Model\Event\Event;
 use CultuurNet\UDB3\Model\Import\JsonImporterInterface;
-use CultuurNet\UDB3\Model\Place\ImmutablePlace;
+use CultuurNet\UDB3\Model\ValueObject\Taxonomy\Category\Category;
+use CultuurNet\UDB3\Model\ValueObject\Taxonomy\Category\CategoryDomain;
 use CultuurNet\UDB3\ReadModel\JsonDocument;
 use CultuurNet\UDB3\Theme;
 use CultuurNet\UDB3\Title;
-use Respect\Validation\Exceptions\GroupedValidationException;
 use Respect\Validation\Exceptions\ValidationException;
 use Symfony\Component\Serializer\Serializer;
 use ValueObjects\StringLiteral\StringLiteral;
 
-/**
- * @todo Move validation errors to a separate EventImportValidator so they can be combined
- * with the the errors from EventValidator.
- */
 class EventJsonImporter implements JsonImporterInterface
 {
     /**
      * @var DocumentRepositoryInterface
      */
     private $eventDocumentRepository;
-
-    /**
-     * @var DocumentRepositoryInterface
-     */
-    private $placeDocumentRepository;
 
     /**
      * @var Serializer
@@ -58,12 +48,10 @@ class EventJsonImporter implements JsonImporterInterface
 
     public function __construct(
         DocumentRepositoryInterface $eventDocumentRepository,
-        DocumentRepositoryInterface $placeDocumentRepository,
         Serializer $serializer,
         CommandBusInterface $commandBus
     ) {
         $this->eventDocumentRepository = $eventDocumentRepository;
-        $this->placeDocumentRepository = $placeDocumentRepository;
         $this->serializer = $serializer;
         $this->commandBus = $commandBus;
     }
@@ -76,9 +64,6 @@ class EventJsonImporter implements JsonImporterInterface
         $id = $jsonDocument->getId();
         $data = $jsonDocument->getRawBody();
 
-        /* @var Event $import */
-        $import = $this->serializer->deserialize($data, Event::class, 'json');
-
         try {
             $currentDocument = $this->eventDocumentRepository->get($id);
 
@@ -89,9 +74,12 @@ class EventJsonImporter implements JsonImporterInterface
             );
         } catch (DocumentGoneException $e) {
             throw new ValidationException('The Event with the given id has been deleted and cannot be re-created.');
+        } catch (\Exception $e) {
+            throw new \LogicException('Could not deserialize internal event read model!', 0, $e);
         }
 
-        $errors = [];
+        /* @var Event $import */
+        $import = $this->serializer->deserialize($data, Event::class, 'json');
 
         $mainLanguage = Language::fromUdb3ModelLanguage(
             $import->getMainLanguage()
@@ -103,83 +91,38 @@ class EventJsonImporter implements JsonImporterInterface
             )
         );
 
-        $categories = array_map(
-            function (array $category) {
-                return new Category(
-                    $category['id'],
-                    $category['label'],
-                    $category['domain']
-                );
-            },
-            $import->getTerms()->toArray()
+        $type = $import->getTerms()
+            ->filter(
+                function (Category $term) {
+                    return $term->getDomain()->sameAs(new CategoryDomain('eventtype'));
+                }
+            )
+            ->getFirst();
+        $type = EventType::fromUdb3Model($type);
+
+        $theme = $import->getTerms()
+            ->filter(
+                function (Category $term) {
+                    return $term->getDomain()->sameAs(new CategoryDomain('theme'));
+                }
+            )
+            ->getFirst();
+        $theme = $theme ? Theme::fromUdb3Model($theme) : null;
+
+        $place = $import->getPlaceReference()->getEmbeddedPlace();
+        $placeId = $place->getId();
+        $placeName = $place->getTitle()->getTranslation($place->getTitle()->getOriginalLanguage());
+        $placeAddress = $place->getAddress()->getTranslation($place->getAddress()->getOriginalLanguage());
+
+        $location = new Location(
+            $import->getPlaceReference()->getPlaceId()->toString(),
+            new StringLiteral($placeName->toString()),
+            Address::fromUdb3Model($placeAddress)
         );
-
-        $types = array_filter(
-            $categories,
-            function ($category) {
-                return $category instanceof EventType;
-            }
-        );
-        $type = reset($types);
-
-        $themes = array_filter(
-            $categories,
-            function (Category $category) {
-                return $category instanceof Theme;
-            }
-        );
-        $theme = count($themes) > 0 ? reset($themes) : null;
-
-        $placeId = $import->getPlaceReference()->getPlaceId();
-        $placeJsonDocument = null;
-        $location = null;
-
-        try {
-            $placeJsonDocument = $this->placeDocumentRepository->get($placeId->toString());
-            if ($placeId->sameAs(ImmutablePlace::getDummyLocationId())) {
-                $errors = 'Can not import events with a dummy location.';
-            } elseif (!$placeJsonDocument) {
-                $errors = 'The given location id does not exist.';
-            }
-        } catch (DocumentGoneException $e) {
-            $errors = 'The given location id is deleted and can not be coupled to the event.';
-        }
-
-        // @todo Refactor CreateEvent, Event::create(), and EventCreated to use LocationID instead
-        // of Location. Afterwards this whole block can be simplified.
-        if ($placeJsonDocument) {
-            $placeJson = $placeJsonDocument->getBody();
-
-            $placeMainLanguage = isset($placeJson->mainLanguage) ? $placeJson->mainLanguage : 'nl';
-            $placeName = $placeJson->name;
-            if (is_array($placeName)) {
-                $placeName = $placeName[$placeMainLanguage];
-            }
-
-            $placeAddress = $placeJson->address;
-            if (is_array($placeAddress)) {
-                $placeAddress = $placeAddress[$placeMainLanguage];
-            }
-
-            $location = new Location(
-                $import->getPlaceReference()->getPlaceId()->toString(),
-                new StringLiteral($placeName),
-                Address::deserialize($placeAddress)
-            );
-        }
 
         $calendar = Calendar::fromUdb3ModelCalendar($import->getCalendar());
 
         $publishDate = $import->getAvailableFrom();
-
-        if (!empty($errors)) {
-            $groupedException = new GroupedValidationException('event');
-            foreach ($errors as $error) {
-                $exception = new ValidationException($error);
-                $groupedException->addRelated($exception);
-            }
-            throw new $groupedException;
-        }
 
         $commands = [];
         if (!$current) {
--- src/Event/PreProcessingEventJsonImporter.php
+++ src/Event/PreProcessingEventJsonImporter.php
@@ -2,9 +2,12 @@
 
 namespace CultuurNet\UDB3\Model\Import\Event;
 
+use CultuurNet\UDB3\Event\ReadModel\DocumentRepositoryInterface;
 use CultuurNet\UDB3\Model\Import\JsonImporterInterface;
 use CultuurNet\UDB3\Model\Import\Taxonomy\Category\CategoryResolverInterface;
+use CultuurNet\UDB3\Model\ValueObject\Identity\UUIDParser;
 use CultuurNet\UDB3\Model\ValueObject\Taxonomy\Category\CategoryID;
+use CultuurNet\UDB3\Model\ValueObject\Web\Url;
 use CultuurNet\UDB3\ReadModel\JsonDocument;
 use Respect\Validation\Exceptions\ValidationException;
 
@@ -21,15 +24,31 @@ class PreProcessingEventJsonImporter implements JsonImporterInterface
     private $categoryResolver;
 
     /**
+     * @var UUIDParser
+     */
+    private $placeIdParser;
+
+    /**
+     * @var DocumentRepositoryInterface
+     */
+    private $placeDocumentRepository;
+
+    /**
      * @param JsonImporterInterface $jsonImporter
      * @param CategoryResolverInterface $categoryResolver
+     * @param UUIDParser $placeIdParser
+     * @param DocumentRepositoryInterface $placeDocumentRepository
      */
     public function __construct(
         JsonImporterInterface $jsonImporter,
-        CategoryResolverInterface $categoryResolver
+        CategoryResolverInterface $categoryResolver,
+        UUIDParser $placeIdParser,
+        DocumentRepositoryInterface $placeDocumentRepository
     ) {
         $this->jsonImporter = $jsonImporter;
         $this->categoryResolver = $categoryResolver;
+        $this->placeIdParser = $placeIdParser;
+        $this->placeDocumentRepository = $placeDocumentRepository;
     }
 
     /**
@@ -65,6 +84,24 @@ class PreProcessingEventJsonImporter implements JsonImporterInterface
                 },
                 $data['terms']
             );
+        }
+
+        // Attempt to add or correct the embedded place data.
+        if (isset($data['location']['@id']) && is_string($data['location']['@id'])) {
+            try {
+                $url = new Url((string) $data['location']['@id']);
+                $placeId = $this->placeIdParser->fromUrl($url);
+                $placeJsonDocument = $this->placeDocumentRepository->get($placeId->toString());
+
+                $placeJson = json_decode($placeJsonDocument->getRawBody(), true);
+                $placeJson = $placeJson ? $placeJson : [];
+                $placeJson['@id'] = $url->toString();
+
+                $data['location'] = $placeJson;
+            } catch (\Exception $e) {
+                // Do nothing. Validators will report the invalid data that
+                // caused the exception later on.
+            }
         }
 
         $jsonDocument = new JsonDocument(
