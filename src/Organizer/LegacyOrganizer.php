706b1aa78874973d6560f3f0f26519ab9d24fd29
Luc Wollants <luc.wollants@apptite.be>
2018-03-22 15:18:51 +0100

    Revert "III-2563 Add getLabels method to LegacyOrganizer."
    This reverts commit 207ddcda54e9084dff0de85b912875334b1d6ab4.

--- src/Organizer/LegacyOrganizer.php
+++ src/Organizer/LegacyOrganizer.php
@@ -2,7 +2,6 @@
 
 namespace CultuurNet\UDB3\Model\Import\Organizer;
 
-use CultuurNet\UDB3\LabelCollection;
 use CultuurNet\UDB3\Language;
 use CultuurNet\UDB3\Title;
 use ValueObjects\Web\Url;
@@ -34,9 +33,4 @@ interface LegacyOrganizer
      *   Language code as key, and Title as value.
      */
     public function getTitleTranslations();
-
-    /**
-     * @return LabelCollection
-     */
-    public function getLabels();
 }
