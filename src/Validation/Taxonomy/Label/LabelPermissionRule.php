a48e58069acbe89c0d5f03c33d9d5fad50cb6ca3
Luc Wollants <luc.wollants@apptite.be>
2018-03-22 14:22:59 +0100

    III-2563 Add LabelPreProcessingDocumentImporter to supplement DecodedDocument with missing UDB3 labels.

---
+++ src/Validation/Taxonomy/Label/LabelPermissionRule.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace CultuurNet\UDB3\Model\Import\Validation\Taxonomy\Label;
+
+use CultuurNet\UDB3\Label\ReadModels\JSON\Repository\ReadRepositoryInterface;
+use CultuurNet\UDB3\Model\ValueObject\Taxonomy\Label\Label;
+use CultuurNet\UDB3\Security\UserIdentificationInterface;
+use Respect\Validation\Rules\AbstractRule;
+use ValueObjects\StringLiteral\StringLiteral;
+
+class LabelPermissionRule extends AbstractRule
+{
+    /**
+     * @var UserIdentificationInterface
+     */
+    private $userIdentification;
+
+    /**
+     * @var ReadRepositoryInterface
+     */
+    private $labelRepository;
+
+    /**
+     * @param UserIdentificationInterface $userIdentification
+     * @param ReadRepositoryInterface $labelRepository
+     */
+    public function __construct(
+        UserIdentificationInterface $userIdentification,
+        ReadRepositoryInterface $labelRepository
+    ) {
+        $this->userIdentification = $userIdentification;
+        $this->labelRepository = $labelRepository;
+    }
+
+    /**
+     * @param Label $input
+     * @return bool
+     */
+    public function validate($input)
+    {
+        if (!($input instanceof Label)) {
+            return false;
+        }
+
+        return $this->labelRepository->canUseLabel(
+            $this->userIdentification->getId(),
+            new StringLiteral($input->getName()->toString())
+        );
+    }
+}
