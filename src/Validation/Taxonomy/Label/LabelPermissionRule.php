3019de01f18550e626cb340320ff7c4f78a6e625
Luc Wollants <luc.wollants@apptite.be>
2018-03-28 09:35:18 +0200

    III-2563 Check label relations for presence of label, for existing labels no security check is needed.

--- src/Validation/Taxonomy/Label/LabelPermissionRule.php
+++ src/Validation/Taxonomy/Label/LabelPermissionRule.php
@@ -2,7 +2,8 @@
 
 namespace CultuurNet\UDB3\Model\Import\Validation\Taxonomy\Label;
 
-use CultuurNet\UDB3\Label\ReadModels\JSON\Repository\ReadRepositoryInterface;
+use CultuurNet\UDB3\Label\ReadModels\JSON\Repository\ReadRepositoryInterface as LabelsRepository;
+use CultuurNet\UDB3\Label\ReadModels\Relations\Repository\ReadRepositoryInterface as LabelRelationsRepository;
 use CultuurNet\UDB3\Model\ValueObject\Taxonomy\Label\Label;
 use CultuurNet\UDB3\Security\UserIdentificationInterface;
 use Respect\Validation\Rules\AbstractRule;
@@ -11,25 +12,41 @@ use ValueObjects\StringLiteral\StringLiteral;
 class LabelPermissionRule extends AbstractRule
 {
     /**
+     * @var string
+     */
+    private $documentId;
+
+    /**
      * @var UserIdentificationInterface
      */
     private $userIdentification;
 
     /**
-     * @var ReadRepositoryInterface
+     * @var LabelsRepository
      */
-    private $labelRepository;
+    private $labelsRepository;
 
     /**
+     * @var LabelRelationsRepository
+     */
+    private $labelRelationsRepository;
+
+    /**
+     * @param StringLiteral $documentId
      * @param UserIdentificationInterface $userIdentification
-     * @param ReadRepositoryInterface $labelRepository
+     * @param LabelsRepository $labelsRepository
+     * @param LabelRelationsRepository $labelsRelationsRepository
      */
     public function __construct(
+        StringLiteral $documentId,
         UserIdentificationInterface $userIdentification,
-        ReadRepositoryInterface $labelRepository
+        LabelsRepository $labelsRepository,
+        LabelRelationsRepository $labelsRelationsRepository
     ) {
+        $this->documentId = $documentId;
         $this->userIdentification = $userIdentification;
-        $this->labelRepository = $labelRepository;
+        $this->labelsRepository = $labelsRepository;
+        $this->labelRelationsRepository = $labelsRelationsRepository;
     }
 
     /**
@@ -38,19 +55,26 @@ class LabelPermissionRule extends AbstractRule
      */
     public function validate($input)
     {
+        // Only possible to validate a Label object.
         if (!($input instanceof Label)) {
             return false;
         }
 
+        // A god user can use every label.
         if ($this->userIdentification->isGodUser()) {
             return true;
         }
 
-        // @todo: If the label was already present no check needed.
-        // Inject the label relations projector to check if label is present
-        // @see \CultuurNet\UDB3\Label\ReadModels\Relations\Repository\ReadRepositoryInterface
-        // Also needs the item id, how to get the item id?
-        return $this->labelRepository->canUseLabel(
+        // If the label is already present on the item no permission check is needed.
+        $labelRelations = $this->labelRelationsRepository->getLabelRelationsForItem($this->documentId);
+        foreach ($labelRelations as $labelRelation) {
+            if ($labelRelation->getLabelName()->toNative() === $input->getName()->toString()) {
+                return true;
+            }
+        }
+
+        // The label is not yet present on the item, do a permission check for the active user.
+        return $this->labelsRepository->canUseLabel(
             $this->userIdentification->getId(),
             new StringLiteral($input->getName()->toString())
         );
--- tests/Validation/Taxonomy/Label/LabelPermissionRuleTest.php
+++ tests/Validation/Taxonomy/Label/LabelPermissionRuleTest.php
@@ -2,7 +2,10 @@
 
 namespace CultuurNet\UDB3\Model\Import\Validation\Taxonomy\Label;
 
-use CultuurNet\UDB3\Label\ReadModels\JSON\Repository\ReadRepositoryInterface;
+use CultuurNet\UDB3\Label\ReadModels\JSON\Repository\ReadRepositoryInterface as LabelsRepository;
+use CultuurNet\UDB3\Label\ReadModels\Relations\Repository\LabelRelation;
+use CultuurNet\UDB3\Label\ReadModels\Relations\Repository\ReadRepositoryInterface as LabelRelationsRepository;
+use CultuurNet\UDB3\Label\ValueObjects\RelationType;
 use CultuurNet\UDB3\Model\ValueObject\Taxonomy\Label\Label;
 use CultuurNet\UDB3\Model\ValueObject\Taxonomy\Label\LabelName;
 use CultuurNet\UDB3\Security\UserIdentificationInterface;
@@ -11,14 +14,24 @@ use ValueObjects\StringLiteral\StringLiteral;
 class LabelPermissionRuleTest extends \PHPUnit_Framework_TestCase
 {
     /**
+     * @var StringLiteral
+     */
+    private $documentId;
+
+    /**
      * @var UserIdentificationInterface|\PHPUnit_Framework_MockObject_MockObject
      */
     private $userIdentification;
 
     /**
-     * @var ReadRepositoryInterface|\PHPUnit_Framework_MockObject_MockObject
+     * @var LabelsRepository|\PHPUnit_Framework_MockObject_MockObject
      */
-    private $labelRepository;
+    private $labelsRepository;
+
+    /**
+     * @var LabelRelationsRepository|\PHPUnit_Framework_MockObject_MockObject
+     */
+    private $labelRelationsRepository;
 
     /**
      * @ver LabelPermissionRule
@@ -27,13 +40,19 @@ class LabelPermissionRuleTest extends \PHPUnit_Framework_TestCase
 
     protected function setUp()
     {
+        $this->documentId = new StringLiteral('f32227be-a621-4cbd-8803-19762d7f9a23');
+
         $this->userIdentification = $this->createMock(UserIdentificationInterface::class);
 
-        $this->labelRepository = $this->createMock(ReadRepositoryInterface::class);
+        $this->labelsRepository = $this->createMock(LabelsRepository::class);
+
+        $this->labelRelationsRepository = $this->createMock(LabelRelationsRepository::class);
 
         $this->labelPermissionRule = new LabelPermissionRule(
+            $this->documentId,
             $this->userIdentification,
-            $this->labelRepository
+            $this->labelsRepository,
+            $this->labelRelationsRepository
         );
     }
 
@@ -45,7 +64,7 @@ class LabelPermissionRuleTest extends \PHPUnit_Framework_TestCase
         $this->userIdentification->expects($this->never())
             ->method('isGodUser');
 
-        $this->labelRepository->expects($this->never())
+        $this->labelsRepository->expects($this->never())
             ->method('canUseLabel');
 
         $this->assertFalse(
@@ -62,7 +81,7 @@ class LabelPermissionRuleTest extends \PHPUnit_Framework_TestCase
             ->method('isGodUser')
             ->willReturn(true);
 
-        $this->labelRepository->expects($this->never())
+        $this->labelsRepository->expects($this->never())
             ->method('canUseLabel');
 
         $this->assertTrue(
@@ -75,7 +94,7 @@ class LabelPermissionRuleTest extends \PHPUnit_Framework_TestCase
     /**
      * @test
      */
-    public function it_delegates_validation_to_label_repository_for_non_god_user()
+    public function it_does_not_delegates_validation_to_label_repository_for_existing_labels_and_non_god_user()
     {
         $userId = new StringLiteral('user_id');
 
@@ -83,14 +102,68 @@ class LabelPermissionRuleTest extends \PHPUnit_Framework_TestCase
             ->method('isGodUser')
             ->willReturn(false);
 
-        $this->userIdentification->expects($this->once())
-            ->method('getId')
-            ->willReturn($userId);
+        $this->labelRelationsRepository->expects($this->once())
+            ->method('getLabelRelationsForItem')
+            ->with($this->documentId)
+            ->willReturn([
+                new LabelRelation(
+                    new \CultuurNet\UDB3\Label\ValueObjects\LabelName('foo'),
+                    RelationType::EVENT(),
+                    $this->documentId,
+                    false
+                ),
+                new LabelRelation(
+                    new \CultuurNet\UDB3\Label\ValueObjects\LabelName('bar'),
+                    RelationType::EVENT(),
+                    $this->documentId,
+                    false
+                ),
+            ]);
 
-        $this->labelRepository->expects($this->once())
+        $this->labelsRepository->expects($this->never())
+            ->method('canUseLabel');
+
+        $this->userIdentification->expects($this->never())
+            ->method('getId');
+
+        $this->assertTrue(
+            $this->labelPermissionRule->validate(
+                new Label(new LabelName('foo'), true)
+            )
+        );
+    }
+
+    /**
+     * @test
+     */
+    public function it_delegates_validation_to_label_repository_for_new_labels_and_non_god_user()
+    {
+        $userId = new StringLiteral('user_id');
+
+        $this->userIdentification->expects($this->once())
+            ->method('isGodUser')
+            ->willReturn(false);
+
+        $this->labelRelationsRepository->expects($this->once())
+            ->method('getLabelRelationsForItem')
+            ->with($this->documentId)
+            ->willReturn([
+                new LabelRelation(
+                    new \CultuurNet\UDB3\Label\ValueObjects\LabelName('bar'),
+                    RelationType::EVENT(),
+                    $this->documentId,
+                    false
+                ),
+            ]);
+
+        $this->labelsRepository->expects($this->once())
             ->method('canUseLabel')
             ->with($userId, new StringLiteral('foo'))
             ->willReturn(true);
+
+        $this->userIdentification->expects($this->once())
+            ->method('getId')
+            ->willReturn($userId);
 
         $this->assertTrue(
             $this->labelPermissionRule->validate(
