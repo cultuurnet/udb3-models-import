8334bbb4e684f1efe5cbf3094f38478f7bc6568f
Bert Ramakers <bertramakers@me.com>
2018-03-30 09:47:00 +0200

    III-2565: Improve and test the MediaManagerImageCollectionFactory

--- src/MediaObject/MediaManagerImageCollectionFactory.php
+++ src/MediaObject/MediaManagerImageCollectionFactory.php
@@ -6,8 +6,10 @@ use CultuurNet\UDB3\Language;
 use CultuurNet\UDB3\Media\Image;
 use CultuurNet\UDB3\Media\ImageCollection;
 use CultuurNet\UDB3\Media\MediaManagerInterface;
+use CultuurNet\UDB3\Media\MediaObjectNotFoundException;
 use CultuurNet\UDB3\Media\Properties\CopyrightHolder;
 use CultuurNet\UDB3\Media\Properties\Description;
+use CultuurNet\UDB3\Media\Properties\MIMEType;
 use CultuurNet\UDB3\Model\ValueObject\MediaObject\MediaObjectReference;
 use CultuurNet\UDB3\Model\ValueObject\MediaObject\MediaObjectReferences;
 use CultuurNet\UDB3\Model\ValueObject\MediaObject\MediaObjectType;
@@ -43,7 +45,22 @@ class MediaManagerImageCollectionFactory implements ImageCollectionFactory
             function (MediaObjectReference $mediaObjectReference) {
                 $id = $mediaObjectReference->getMediaObjectId();
 
-                $mediaObjectAggregate = $this->mediaManager->get(new UUID($id->toString()));
+                try {
+                    $mediaObjectAggregate = $this->mediaManager->get(new UUID($id->toString()));
+                } catch (MediaObjectNotFoundException $e) {
+                    return null;
+                }
+
+                $allowedMimeTypes = [
+                    MIMEType::fromSubtype('jpeg'),
+                    MIMEType::fromSubtype('png'),
+                    MIMEType::fromSubtype('gif'),
+                ];
+                $mimeType = $mediaObjectAggregate->getMimeType();
+
+                if (!in_array($mimeType, $allowedMimeTypes)) {
+                    return null;
+                }
 
                 return new Image(
                     $mediaObjectAggregate->getMediaObjectId(),
@@ -56,6 +73,8 @@ class MediaManagerImageCollectionFactory implements ImageCollectionFactory
             },
             $mediaObjectsReferences->toArray()
         );
+
+        $images = array_filter($images);
 
         return ImageCollection::fromArray($images);
     }
