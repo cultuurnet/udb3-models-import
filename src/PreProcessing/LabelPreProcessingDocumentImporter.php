321340ff064f64f3cfcc5207e934934403f95b4d
Luc Wollants <luc.wollants@apptite.be>
2018-04-11 11:40:10 +0200

    III-2575 Make sure to create the newly imported labels as fully functional udb3 labels.

--- src/PreProcessing/LabelPreProcessingDocumentImporter.php
+++ src/PreProcessing/LabelPreProcessingDocumentImporter.php
@@ -2,9 +2,11 @@
 
 namespace CultuurNet\UDB3\Model\Import\PreProcessing;
 
+use CultuurNet\UDB3\Label\LabelServiceInterface;
 use CultuurNet\UDB3\Label\ReadModels\JSON\Repository\ReadRepositoryInterface as LabelRepository;
 use CultuurNet\UDB3\Label\ReadModels\Relations\Repository\LabelRelation;
 use CultuurNet\UDB3\Label\ReadModels\Relations\Repository\ReadRepositoryInterface as LabelRelationsRepository;
+use CultuurNet\UDB3\Label\ValueObjects\LabelName;
 use CultuurNet\UDB3\Label\ValueObjects\Visibility;
 use CultuurNet\UDB3\Model\Import\DecodedDocument;
 use CultuurNet\UDB3\Model\Import\DocumentImporterInterface;
@@ -28,18 +30,26 @@ class LabelPreProcessingDocumentImporter implements DocumentImporterInterface
     private $labelRelationsRepository;
 
     /**
+     * @var LabelServiceInterface
+     */
+    private $labelService;
+
+    /**
      * @param DocumentImporterInterface $jsonImporter
      * @param LabelRepository $labelsRepository
      * @param LabelRelationsRepository $labelRelationsRepository
+     * @param LabelServiceInterface $labelService
      */
     public function __construct(
         DocumentImporterInterface $jsonImporter,
         LabelRepository $labelsRepository,
-        LabelRelationsRepository $labelRelationsRepository
+        LabelRelationsRepository $labelRelationsRepository,
+        LabelServiceInterface $labelService
     ) {
         $this->jsonImporter = $jsonImporter;
         $this->labelsRepository = $labelsRepository;
         $this->labelRelationsRepository = $labelRelationsRepository;
+        $this->labelService = $labelService;
     }
 
     /**
@@ -53,7 +63,8 @@ class LabelPreProcessingDocumentImporter implements DocumentImporterInterface
         // Approach is to:
         //  1. get all UDB3 labels from label relation (hidden and visible)
         //  2. remove all UDB3 labels from document (both hidden and visible)
-        //  3. add all UDB3 labels to document (both hidden and visible)
+        //  3. create the potential new labels (these can only come from the imported labels)
+        //  4. add all UDB3 labels to document (both hidden and visible)
         // By using this approach the correct visible/invisible label state
         // is taken into account and the JSON document is correct.
 
@@ -85,7 +96,21 @@ class LabelPreProcessingDocumentImporter implements DocumentImporterInterface
             $udb3Labels
         );
 
-        //  3. add all UDB3 labels to document (both hidden and visible)
+        //  3. create the potential new labels (these can only come from the imported labels)
+        foreach ($data['labels'] as $label) {
+            $this->labelService->createLabelAggregateIfNew(
+                new LabelName($label),
+                true
+            );
+        }
+        foreach ($data['hiddenLabels'] as $label) {
+            $this->labelService->createLabelAggregateIfNew(
+                new LabelName($label),
+                false
+            );
+        }
+
+        //  4. add all UDB3 labels to document (both hidden and visible)
         foreach ($udb3LabelRelations as $udb3LabelRelation) {
             // @todo: what if inside label relations but not inside label repo?
             $label = $this->labelsRepository->getByName($udb3LabelRelation->getLabelName());
